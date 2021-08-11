e9713d66484569e7faf3c038a74068eae10071bc
Eugene Gian <eugene.gian@outlook.com>
2021-08-10 23:07:40 -0700

    added gradual square color to black after 10 passes

--- script.js
+++ script.js
@@ -3,13 +3,17 @@ const gridEl = document.querySelector(".grid");
 const resetBtn = document.querySelector(".resetBtn");
 
 function colorSquare() {
-  console.log(this);
-  this.style.backgroundColor = "black";
+  const randomColor = Math.floor(Math.random() * 16777215).toString(16);
+  this.style.backgroundColor = "#" + randomColor;
+  if (!this.style.opacity) {
+    this.style.opacity = 1;
+  } else {
+    this.style.opacity = this.style.opacity -= 0.1;
+  }
 }
 
 //why doesn't this work? what is this keyword referring to?
 // const colorSquare = () => {
-//   console.log(this);
 //   this.style.backgroundColor = "black";
 // };
 
@@ -17,7 +21,7 @@ const createGrid = (gridNum) => {
   for (let i = 0; i < gridNum ** 2; i++) {
     let div = document.createElement("div");
     div.classList.add("box");
-    div.addEventListener("mouseover", colorSquare, { once: true });
+    div.addEventListener("mouseover", colorSquare);
     gridEl.appendChild(div);
     gridEl.style.gridTemplateColumns = `repeat(${gridNum}, 1fr)`;
     gridEl.style.gridTemplateRows = `repeat(${gridNum}, 1fr)`;
@@ -42,3 +46,7 @@ const resetGame = function () {
 resetBtn.addEventListener("click", resetGame);
 
 createGrid(16);
+
+//(Optional): Instead of just changing the color of a square from black to white (for example), have each pass through with the mouse change it to a completely random RGB value. Then try having each pass just add another 10% of black to it so that only after 10 passes is the square completely black.
+//Math.random rgb
+//need to add 10% black every time. Or perhaps reduce opacity by .1?
